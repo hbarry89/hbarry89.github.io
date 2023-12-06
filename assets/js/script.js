5e497066fccec728f2299a141c5739202cc48901
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-12-05 21:44:33 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,22 @@
 
 let toggleButton = document.getElementById('toggle-button');
 let body = document.body;
-let skills = document.querySelector('.skills-wrapper');
 let card = document.querySelectorAll('.card');
-let hoverEffects = document.querySelectorAll('.hover-effect');
+let iconLinks = document.querySelectorAll('.icon-links');
 
 toggleButton.addEventListener('click', function () {
     body.classList.toggle('dark-mode');
     card.forEach(function (card) {
         card.classList.toggle('dark-card');
     });
+    iconLinks.forEach(function (iconLink) {
+        iconLink.classList.toggle('dark-icon-links');
+    });
 });
 
 // --- Hover ---
 
+let hoverEffects = document.querySelectorAll('.hover-effect');
 hoverEffects.forEach(function(hoverEffect) {
     hoverEffect.addEventListener('mouseover', function() {
         if (body.classList.contains('dark-mode')) {
