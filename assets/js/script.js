28c3cd6e65609d028b28612e2d79e7281108e510
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-12-26 22:45:57 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,27 @@
 
 let toggleButton = document.getElementById('toggle-button');
 let body = document.body;
-let card = document.querySelectorAll('.card');
-let iconLinks = document.querySelectorAll('.icon-links');
 
-toggleButton.addEventListener('click', function () {
-    body.classList.toggle('dark-mode');
-    card.forEach(function (card) {
-        card.classList.toggle('dark-card');
-    });
-    iconLinks.forEach(function (iconLink) {
-        iconLink.classList.toggle('dark-icon-links');
-    });
+toggleButton.addEventListener('click', function() {
+    let currentMode = body.getAttribute('data-bs-theme');
+    if (currentMode === 'light') {
+        body.setAttribute('data-bs-theme', 'dark');
+    } else {
+        body.setAttribute('data-bs-theme', 'light');
+    }
 });
 
-// --- Hover ---
+// Hover
 
 let hoverEffects = document.querySelectorAll('.hover-effect');
+
 hoverEffects.forEach(function(hoverEffect) {
     hoverEffect.addEventListener('mouseover', function() {
-        if (body.classList.contains('dark-mode')) {
-            hoverEffect.style.backgroundColor = '#2b3035';
-        } else if (!body.classList.contains('dark-mode')) {
+        let currentMode = body.getAttribute('data-bs-theme');
+        if (currentMode === 'light') {
             hoverEffect.style.backgroundColor = '#f8f9fa';
+        } else if (currentMode === 'dark') {
+            hoverEffect.style.backgroundColor = '#2b3035';
         }
     });
 
